21ab8ab92ebfdc54dafa756cd3d5e566164aeb07
Ajim Ullah <158139375+ajimullah@users.noreply.github.com>
2024-02-15 10:28:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var names=["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+ names.forEach((val)=>{
+     if (val[0]==="J"){
+         console.log("Good Bye",val);
+     }else{
+         console.log("Hello",val);
+     };
+ });
